a6f96965c94c1d23fc4816c531c3f9b65bd676ac
Andreyka445 <seleznevandrey186@outlook.com>
2026-08-11 14:28:03 +0000

    Galaxian: vendor: Sync with dt
    Signed-off-by: Andreyka445 <seleznevandrey186@outlook.com>

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),Galaxian)
+
+$(call add-radio-file-sha1-checked,radio/apusys.img,3d851dc97af4fa01f5ac3d487bcf5ada7da5dd03)
+$(call add-radio-file-sha1-checked,radio/ccu.img,4d1e4097a5b3042697984cc5e8b336332b53fa44)
+$(call add-radio-file-sha1-checked,radio/connsys_bt.img,857092aaa0fd12caec3bdbc85efaf70f178df1a0)
+$(call add-radio-file-sha1-checked,radio/connsys_gnss.img,550f5c75c99318a5c283f24394cd8ea1208de801)
+$(call add-radio-file-sha1-checked,radio/connsys_wifi.img,1ea67f06379a72b087eca9fa2328cc617bdca856)
+$(call add-radio-file-sha1-checked,radio/dpm.img,3a6bc33a40b5651a5ef69eefc61d73201310800c)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,8bc9c680a51953b97b36977ec310918b75500834)
+$(call add-radio-file-sha1-checked,radio/gz.img,eb766634c395782dea6898fa212a736803753b09)
+$(call add-radio-file-sha1-checked,radio/lk.img,3c438fa431ca14705f392f7de3571f659994105d)
+$(call add-radio-file-sha1-checked,radio/logo.img,141aa45e895d1930d4ec3ea768c4aca3c8aca033)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,65e0d3d8f752236239dd70168c90d211cbb811b4)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,8b2d802eaa7f1430064640e6e256dafd5f0ef8d6)
+$(call add-radio-file-sha1-checked,radio/modem.img,cefa691b5b503a979bd0f47502131ae53a536355)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,971d62d679d754954cdc8fd030350b56f225c213)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,6ed4c334c444e9e00a27d31d9bede4808814bd61)
+$(call add-radio-file-sha1-checked,radio/scp.img,24031eb4ee599aadac94ec4fe0d8ee7d271cfc33)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,d4aed70619e521ac41a852598579f66a50e1330a)
+$(call add-radio-file-sha1-checked,radio/sspm.img,8a09929ec5b342eeebda3fe3e5c9f22db2322598)
+$(call add-radio-file-sha1-checked,radio/tee.img,9cb9560eba13cabc5df9db166904470209d818d2)
+$(call add-radio-file-sha1-checked,radio/vcp.img,673f45363a216b5fccc898c792073b9c2c73174e)
+
+endif
